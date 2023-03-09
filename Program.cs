16eb7e5437e25f21389e372f96f3d937cfcbb604
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-09 22:33:55 +0530

    Add : Solved Simulate Balanced Paranthesis Problem

--- Program.cs
+++ Program.cs
@@ -8,27 +8,125 @@ namespace AlgorithmProblemDemo
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public class stack
         {
+            public int top = -1;
+            public char[] items = new char[100];
 
-            string string1, rev;
-            Console.WriteLine("Enter the String to check is Palindrome or not ");
-            string1 = Console.ReadLine();
-            string s1 = string1.ToLower();
-            char[] ch = s1.ToCharArray();
-            Array.Reverse(ch);
-            rev = new string(ch);
-            bool b = s1.Equals(rev, StringComparison.OrdinalIgnoreCase);
-            if (b == true)
+            public void push(char x)
             {
-                Console.WriteLine("" + string1 + " is a Palindrome!");
+                if (top == 99)
+                {
+                    Console.WriteLine("Stack full");
+                }
+                else
+                {
+                    items[++top] = x;
+                }
             }
+
+            char pop()
+            {
+                if (top == -1)
+                {
+                    Console.WriteLine("Underflow error");
+                    return '\0';
+                }
+                else
+                {
+                    char element = items[top];
+                    top--;
+                    return element;
+                }
+            }
+
+            Boolean isEmpty()
+            {
+                return (top == -1) ? true : false;
+            }
+        }
+
+        // Returns true if character1 and character2
+        // are matching left and right brackets */
+        static Boolean isMatchingPair(char character1,
+                                      char character2)
+        {
+            if (character1 == '(' && character2 == ')')
+                return true;
+            else if (character1 == '{' && character2 == '}')
+                return true;
+            else if (character1 == '[' && character2 == ']')
+                return true;
+            else
+                return false;
+        }
+
+        // Return true if expression has balanced
+        // Brackets
+        static Boolean areBracketsBalanced(char[] exp)
+        {
+            // Declare an empty character stack */
+            Stack<char> st = new Stack<char>();
+
+            // Traverse the given expression to
+            //   check matching brackets
+            for (int i = 0; i < exp.Length; i++)
+            {
+                // If the exp[i] is a starting
+                // bracket then push it
+                if (exp[i] == '{' || exp[i] == '('
+                    || exp[i] == '[')
+                    st.Push(exp[i]);
+
+                //  If exp[i] is an ending bracket
+                //  then pop from stack and check if the
+                //   popped bracket is a matching pair
+                if (exp[i] == '}' || exp[i] == ')'
+                    || exp[i] == ']')
+                {
+
+                    // If we see an ending bracket without
+                    //   a pair then return false
+                    if (st.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    // Pop the top element from stack, if
+                    // it is not a pair brackets of
+                    // character then there is a mismatch. This
+                    // happens for expressions like {(})
+                    else if (!isMatchingPair(st.Pop(),
+                                             exp[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // If there is something left in expression
+            // then there is a starting bracket without
+            // a closing bracket
+
+            if (st.Count == 0)
+                return true; // balanced
             else
             {
-                Console.WriteLine(" " + string1 + " is not a Palindrome!");
+                // not balanced
+                return false;
             }
-    
+        }
 
+        // Driver code
+        public static void Main(String[] args)
+        {
+            char[] exp = { '{', '(', ')', '}', '[', ']' };
+
+            // Function call
+            if (areBracketsBalanced(exp))
+                Console.WriteLine("Balanced ");
+            else
+                Console.WriteLine("Not Balanced ");
         }
     }
 }
