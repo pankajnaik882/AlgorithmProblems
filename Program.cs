82fd358080c1373df3b8c7071d5299035d1e4f6d
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-09 22:02:46 +0530

    Add : Solved Permutation Of String

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace AlgorithmProblemDemo
+{
+    public class Program
+    {
+        public static void swap(char[] chars, int i, int j)
+        {
+            char temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+        }
+
+        public static void permutations(char[] chars, int currentIndex)
+        {
+            if (currentIndex == chars.Length - 1)
+            {
+               Console.WriteLine(chars);
+            }
+
+            for (int i = currentIndex; i < chars.Length; i++)
+            {
+                swap(chars, currentIndex, i);
+                permutations(chars, currentIndex + 1);
+                swap(chars, currentIndex, i);
+            }
+        }
+
+        public static void findPermutations(String str)
+        {
+
+            // base case
+            if (str == null || str.Length == 0)
+            {
+                return;
+            }
+
+            permutations(str.ToCharArray(), 0);
+        }
+
+
+        public static void Main(string[] args)
+        {
+            String str = "ABC";
+            findPermutations(str);
+        }
+    }
+}
