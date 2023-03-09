827be34f770aa09a502faa7ef53aedbd171c6a76
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-09 22:07:14 +0530

    Add : Solved Binary Search Sort Problem

--- Program.cs
+++ Program.cs
@@ -1,46 +1,56 @@
-﻿namespace AlgorithmProblemDemo
+﻿using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlgorithmProblemDemo
 {
+
+   
     public class Program
     {
-        public static void swap(char[] chars, int i, int j)
+        public static void Display(char[] arr)
         {
-            char temp = chars[i];
-            chars[i] = chars[j];
-            chars[j] = temp;
-        }
-
-        public static void permutations(char[] chars, int currentIndex)
-        {
-            if (currentIndex == chars.Length - 1)
+            foreach (char c in arr)
             {
-               Console.WriteLine(chars);
-            }
-
-            for (int i = currentIndex; i < chars.Length; i++)
-            {
-                swap(chars, currentIndex, i);
-                permutations(chars, currentIndex + 1);
-                swap(chars, currentIndex, i);
+                Console.WriteLine(c + " ");
             }
         }
-
-        public static void findPermutations(String str)
-        {
-
-            // base case
-            if (str == null || str.Length == 0)
-            {
-                return;
-            }
-
-            permutations(str.ToCharArray(), 0);
-        }
-
-
         public static void Main(string[] args)
         {
-            String str = "ABC";
-            findPermutations(str);
+            char[] arr = { 'e', 'c','d','e','f'};
+            
+            //For this method array will be sorted.
+            Array.Sort(arr);
+
+            Console.WriteLine("The Element of sorted array : ");
+
+            Display(arr);
+
+            object s = 'h';
+
+            result(arr, s);
+
+            object s1 = 'a';
+
+            result(arr, s1);
+            
         }
+
+        public static void result(char[] arr2, object k)
+        {
+            int res = Array.BinarySearch(arr2, k);
+            if (res < 0)
+            {
+                Console.WriteLine("\nThe element to search for "
+                                + "({0}) is not found.",
+                            k);
+            }
+            else
+            {
+                Console.WriteLine("The element to search for "
+                                      + "({0}) is at index {1}.",
+                                  k, res);
+            }
+        }
+
     }
 }
