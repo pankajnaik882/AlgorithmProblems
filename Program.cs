6463c3d6d62eee74eb17708a803ba6a476622ef6
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-09 22:18:43 +0530

    Add : Solved Merge Sort Problem

--- Program.cs
+++ Program.cs
@@ -1,46 +1,79 @@
 ﻿using System.Collections.Immutable;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 
 namespace AlgorithmProblemDemo
 {
-
-   
     public class Program
     {
-
-        public static void DisplayNumber()
+        static public void MainMerge(int[] numbers, int left, int mid, int right)
         {
+            int[] temp = new int[25];
+            int i, eol, num, pos;
+            eol = (mid - 1);
+            pos = left;
+            num = (right - left + 1);
 
-            int[] arr = { 78, 55, 45, 98, 13 };
-            int temp;
-            for (int j = 0; j <= arr.Length - 2; j++)
+            while ((left <= eol) && (mid <= right))
             {
-                for (int i = 0; i <= arr.Length - 2; i++)
-                {
-                    if (arr[i] > arr[i + 1])
-                    {
-                        temp = arr[i + 1];
-                        arr[i + 1] = arr[i];
-                        arr[i] = temp;
-                    }
-                }
+                if (numbers[left] <= numbers[mid])
+                    temp[pos++] = numbers[left++];
+                else
+                    temp[pos++] = numbers[mid++];
             }
-            Console.WriteLine("BubbleSort Sorted");
-            foreach (int p in arr)
+            while (left <= eol)
+                temp[pos++] = numbers[left++];
+            while (mid <= right)
+                temp[pos++] = numbers[mid++];
+            for (i = 0; i < num; i++)
             {
-                Console.WriteLine(p);
+                numbers[right] = temp[right];
+                right--;
             }
-
         }
 
-
-
-
-        public static void Main(string[] args)
+        static public void SortMerge(int[] numbers, int left, int right)
         {
-            Program.DisplayNumber();
-
-
+            int mid;
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                SortMerge(numbers, left, mid);
+                SortMerge(numbers, (mid + 1), right);
+                MainMerge(numbers, left, (mid + 1), right);
+            }
         }
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("\nProgram for sorting a numeric array using Merge Sorting");
+            Console.Write("\n\nEnter number of elements: ");
+            int max = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[max];
+            for (int i = 0; i < max; i++)
+            {
+                Console.Write("\nEnter [" + (i + 1).ToString() + "] element: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("Input int array : ");
+            Console.Write("\n");
+            for (int k = 0; k < max; k++)
+            {
+                Console.Write(numbers[k] + " ");
+                Console.Write("\n");
+            }
+            Console.WriteLine("MergeSort By Recursive Method");
+            SortMerge(numbers, 0, max - 1);
+            for (int i = 0; i < max; i++)
+                Console.WriteLine(numbers[i]);
+            Console.ReadLine();
+        }
+
     }
 }
+           
+
+        
