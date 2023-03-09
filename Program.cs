0dcc8fd5b758b71c89f2fe70815c5cf92a3b56d6
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-09 22:17:49 +0530

    Add : Solved Bubble Sort Problem

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Immutable;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlgorithmProblemDemo
+{
+
+   
+    public class Program
+    {
+
+        public static void DisplayNumber()
+        {
+
+            int[] arr = { 78, 55, 45, 98, 13 };
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("BubbleSort Sorted");
+            foreach (int p in arr)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+
+
+
+
+        public static void Main(string[] args)
+        {
+            Program.DisplayNumber();
+
+
+        }
+    }
+}
